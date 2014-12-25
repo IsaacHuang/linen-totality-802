a2c18215bfa329c22aba6a1e97fcf80f94ed090e
IsaacHuang <issac11235813@gmail.com>
2014-12-26 01:45:22 +0800

    SA_Project
    PhotoMachines

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    var x = "Version info: " + navigator.appVersion;
+    document.getElementById("demo").innerHTML = x;
+}
--- main.py
+++ main.py
@@ -20,86 +20,20 @@ class MainHandler(webapp2.RequestHandler):
 	def get(self):
 		self.response.write("""
 		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-  			<title>Bootstrap Example</title>
-  			<meta charset="utf-8">
-  			<meta name="viewport" content="width=device-width, initial-scale=1">
-  			<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-  			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  			<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-		</head>
-		<body>
+<html>
+<body onload="myFunction()">
 
-			<div class="container">
-  				<div class="jumbotron">
-    				<h1>My First Bootstrap Page</h1>
-    				<p>Resize this responsive page to see the effect!</p> 
-  				</div>
-  				<div class="row">
-    				<div class="col-sm-4">
-      					<h3>Column 1</h3>
-      					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    				</div>
-    				<div class="col-sm-4">
-      					<h3>Column 2</h3>
-      					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    				</div>
-    				<div class="col-sm-4">
-      					<h3>Column 3</h3>        
-      					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-      					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-    				</div>
-  				</div>
-			</div>
+<p id="demo"></p>
 
-		</body>
-		</html>
-		""")
+<script>
+function myFunction() {
+    var x = "Version info: " + navigator.appVersion;
+    document.getElementById("demo").innerHTML = x;
+}
+</script>
 
-class index(webapp2.RequestHandler):
-  def get(self):
-    self.response.write("""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Bootstrap Example</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-
-      <div class="container">
-          <div class="jumbotron">
-            <h1>My First Bootstrap Page</h1>
-            <p>Resize this responsive page to see the effect!</p> 
-          </div>
-          <div class="row">
-            <div class="col-sm-4">
-                <h3>Column 1</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            </div>
-            <div class="col-sm-4">
-                <h3>Column 2</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            </div>
-            <div class="col-sm-4">
-                <h3>Column 3</h3>        
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            </div>
-          </div>
-      </div>
-
-    </body>
-    </html>
+</body>
+</html>
     """)
 
 app = webapp2.WSGIApplication([
