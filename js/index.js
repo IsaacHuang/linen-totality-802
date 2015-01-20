051894db1e1cc0161a8de6588fe983e0ab44fcbd
IsaacHuang <issac11235813@gmail.com>
2015-01-21 01:55:03 +0800

    SA_project
    opencv-php

--- js/index.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-    var x = "Version info: " + navigator.appVersion;
-    document.getElementById("demo").innerHTML = x;
-}
--- opencv-python/config.py
+++
@@ -1,4 +0,0 @@
-
-from google.appengine.api import app_identity
-
-BUCKET_NAME = '' or app_identity.get_default_gcs_bucket_name()
